2d0eca2de9a86d646178f1c865897af54c06b3ed
paiz4ever <1151892501@qq.com>
2024-08-09 17:09:56 +0800

    feat: 可回收节点类

--- extensions/core/assets/internal/components/Recyclable.ts
+++ extensions/core/assets/internal/components/Recyclable.ts
@@ -1,4 +1,9 @@
 import { _decorator, Component, Node } from "cc";
 const { ccclass, property } = _decorator;
 
-export default abstract class Recyclable extends Component {}
+export default abstract class Recyclable extends Component {
+  /**
+   * 节点被回收后会调用此方法
+   */
+  abstract onRecycle(): void;
+}
--- extensions/core/assets/internal/utils/pool.ts
+++ extensions/core/assets/internal/utils/pool.ts
@@ -1,13 +1,24 @@
-import { NodePool } from "cc";
+import { Node, NodePool } from "cc";
+import Recyclable from "../components/Recyclable";
 
 const pools: Map<string, NodePool> = new Map();
 export function pool(key: string) {
   if (!pools.has(key)) {
     const pool = new NodePool();
-    const originalMethod = pool.clear;
+    const originalClear = pool.clear;
+    const originalPut = pool.put;
     pool.clear = function () {
-      originalMethod.call(this);
+      originalClear.call(this);
       pools.delete(key);
+    };
+    pool.put = function (node: Node) {
+      try {
+        // @ts-ignore
+        node.getComponents(Recyclable).forEach((r) => r.onRecycle());
+      } catch (e) {
+        console.warn("node onRecycle error: ", e);
+      }
+      originalPut.call(this, node);
     };
     pools.set(key, pool);
     return pool;

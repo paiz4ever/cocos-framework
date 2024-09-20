0c4adb24aa72582b3a2dfdc2db49ba4adcb878dc
paiz4ever <1151892501@qq.com>
2024-09-20 11:00:30 +0800

    fix: windows打包原生app为不可拓展对象无法赋值

--- extensions/core/assets/module/index.ts
+++ extensions/core/assets/module/index.ts
@@ -1,10 +1,12 @@
 import { DEBUG } from "cc/env";
 import * as app from "./export";
 
-Object.defineProperty(app, "table", {
+// windows环境下（安卓原生）app是不可拓展对象，即Object.isExtensible(app)为false，因此克隆一份
+const extensibleApp = { ...app };
+Object.defineProperty(extensibleApp, "table", {
   get() {
     return app.config.tables;
   },
 });
-export default app as any;
-if (DEBUG) (window as any)["app"] = app;
+export default extensibleApp as any;
+if (DEBUG) (window as any)["app"] = extensibleApp;
